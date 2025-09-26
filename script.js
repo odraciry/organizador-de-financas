fae734d712148338ef69e790d15e25d4a31f8c5c
Ricardo da Silva Pereira <ricardo.pereira@paxbr.com.br>
2025-09-26 12:32:48 -0300

    localstorage pronto

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ let buttonCreateCard = document.getElementById('buttonCreateCard');
 let closeModal = document.getElementById('closeModal');
 let modalCriacao = document.getElementById('modalCriacao');
 let titleCardInput = document.getElementById('newCardTitle').value;
+let titleCard = document.getElementById('titleCard');
 let btCriarCard = document.getElementById('btCriarCard');
 let btCancelCard = document.getElementById('btCancelCard');
 let cardObjects = [];
 let containerCards = document.getElementById('containerCards');
-let html = ""
+// let iconLixeira = document.getElementById('iconLixeira');
+let html = "";
 
 
 buttonCreateCard.addEventListener('click', () => {
     modalCriacao.style.display = "flex";
     titleCard.value = document.getElementById('newCardTitle').value;
+    console.log(document.getElementById('newCardTitle').value)
 });
 closeModal.addEventListener('click', () => {
     modalCriacao.style.display = "none";
@@ -29,6 +32,7 @@ btCriarCard.addEventListener('click', () => {
     if (mes != '' && mes != 'Invalid Date' && dia > 0 && document.getElementById('titleCard').value != '' && document.getElementById('valueCard').value != '') {
         modalCriacao.style.display = "none";
         cardObjects.push({
+            id: generateId(),
             title: document.getElementById('titleCard').value,
             day: dia,
             month: mes,
@@ -47,10 +51,52 @@ btCriarCard.addEventListener('click', () => {
     }
 
     console.log(cardObjects);
+    salvarCards()
+    carregarCards();
+    resetInputs();
+});
+
+containerCards.addEventListener("click", (e) => {
+    const card = e.target.closest('.contaBox');
+    let cardTargetId = card.querySelector(".idCard").innerText;
+    const checkbox = card.querySelector(".cardCheckbox");
+    const iconLixeira = card.querySelector(".iconLixeira");
+    // se clicou diretamente no checkbox, não faz nada
+    if (e.target.tagName === "INPUT" && e.target.type === "checkbox") return;
+    if (e.target.tagName === "IMG") {
+        alert('ola mundo')
+        cardObjects = cardObjects.filter(card => card.id.toString() !== cardTargetId.toString());
+        console.log(cardObjects)
+        salvarCards();
+        carregarCards();
+    };
+
+
+    if (checkbox.checked) {
+        checkbox.checked = false;
+        iconLixeira.style.display = "none";
+    } else {
+        checkbox.checked = true;
+        iconLixeira.style.display = "block";
+    }
+    checkbox.addEventListener("input", () => {
+        if (checkbox.checked) {
+            checkbox.checked = false;
+            iconLixeira.style.display = "none";
+        } else {
+            checkbox.checked = true;
+            iconLixeira.style.display = "block";
+        }
+    })
+
+})
+
+function pushCards() {
+    // containerCards.innerHTML = "";
+    html = "";
     cardObjects.forEach(card => {
-
-
-        html = `<div class="contaBox">\
+        html += `<div class="contaBox">\
+                <span class="idCard">${card.id}</span>
                 <div class="divContaHeader">\
                     <div>\
                         <span>${card.month}</span>\
@@ -59,31 +105,17 @@ btCriarCard.addEventListener('click', () => {
                     <input type="checkbox" class="cardCheckbox">\
                 </div>\
                 <div class="contaInfos">\
-                    <h2>${card.title}</h2>\
+                    <h2 class="titleCard">${card.title}</h2>\
                 </div>\
                 <div class="contaValores">\
                     <p class="valorConta">R$ ${card.value.replace(".", ",")}</p>\
+                    <img src="img/lixeira.png" alt="" class="iconLixeira">\
                 </div>\
             </div>`
     });
-    containerCards.innerHTML += html;
-    resetInputs();
-});
+    containerCards.innerHTML = html;
 
-containerCards.addEventListener("click", (e) => {
-    const card = e.target.closest('.contaBox');
-    // se clicou diretamente no checkbox, não faz nada
-    if (e.target.tagName === "INPUT" && e.target.type === "checkbox") return;
-
-    const checkbox = card.querySelector(".cardCheckbox");
-    if (checkbox.checked) {
-        checkbox.checked = false;
-    } else {
-        checkbox.checked = true;
-    }
-    console.log(checkbox);
-    console.log("card clicado", card.innerHTML);
-})
+}
 
 function resetInputs() {
     document.getElementById('titleCard').value = "";
@@ -91,3 +123,27 @@ function resetInputs() {
     document.getElementById('valueCard').value = "";
     document.getElementById('dateCard').value = "";
 }
+
+function generateId() {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2, 9);
+}
+
+function salvarCards() {
+  localStorage.setItem("cards", JSON.stringify(cardObjects));
+}
+
+function carregarCards() {
+  cardObjects = JSON.parse(localStorage.getItem("cards")) || [];
+  pushCards(); // redesenha os cards no HTML
+}
+
+
+// carregar os cards quando a página abre
+window.addEventListener("DOMContentLoaded", () => {
+  carregarCards();
+});
+
+// salvar os cards sempre que recarregar/fechar
+window.addEventListener("beforeunload", () => {
+  salvarCards();
+});
